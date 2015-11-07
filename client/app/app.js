8a2511a0df7cc4568696e2ea815ccae5e4ede41c
shashi-dokania <shashi.dokania@gmail.com>
2015-11-06 18:30:34 -0800

    Implement links.js

--- client/app/app.js
+++ client/app/app.js
@@ -15,7 +15,17 @@ angular.module('shortly', [
       templateUrl: 'app/auth/signup.html',
       controller: 'AuthController'
     })
-    // Your code here
+    .when('/links', {
+      templateUrl: 'app/links/links.html',
+      controller: 'LinksController'
+    })
+    // .otherwise({
+    //   redirectTo: '/links'
+    // });
+    .when('/shorten', {
+      templateUrl: 'app/shorten/shorten.html',
+      controller: 'ShortenController'
+    })
 
     // We add our $httpInterceptor into the array
     // of interceptors. Think of it like middleware for your ajax calls
--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,17 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
+  console.log(Links);
   // Your code here
+  $scope.data = {
+    links: []
+  };
+  $scope.getLinks = function() {
+    // $scope.data.links = Links.getLinks();
+    Links.getLinks().then(function (resp) {
+      $scope.data.links = resp.data;
+    })
+  };
+
+  $scope.getLinks();
 });
